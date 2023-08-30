07f1811809335cfbd44084f95dcb5bf0c219bcc8
Thomas Thomassen <thomas@thomthom.net>
2023-08-30 18:12:51 +0200

    Make test app parse input decimal strings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include "roman.h"
 
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 int main(int argc, char* argv[])
 {
-  std::cout << "Hello World\n";
+  std::cout << "Decimal to Roman Numerals\n";
+
+  if (argc <= 1)
+  {
+    std::cout << "Usage: roman DECIMAL...\n";
+    std::cout << "Examples:\n";
+    std::cout << "  roman 1983\n";
+    std::cout << "  roman 1983 123\n";
+    return 0;
+  }
+
+  for (int i = 1; i < argc; ++i)
+  {
+    const auto input = argv[i];
+    const auto decimal = std::stoi(input);
+    const auto numeral = RomanNumeral(decimal);
+    std::cout << std::setw(2) << i << ". " << std::setw(6) << decimal << " => " << numeral.roman()
+              << "\n";
+  }
+  return 0;
 }
